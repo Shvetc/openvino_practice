c71ef11cb17089c520584ef7172a8e337cfcf4c9
Shvec_D <schwets.mitia-shvets@yandex.ru>
2020-07-14 18:15:30 +0300

    had done_nms_iou
    git commit -mhad_done_nms_iou

--- modules/4_detection/src/detector.cpp
+++ modules/4_detection/src/detector.cpp
@@ -13,6 +13,24 @@ Detector::Detector() {
     // Load deep learning network into memory
     auto net = ie.ReadNetwork(utils::fs::join(DATA_FOLDER, "face-detection-0104.xml"),
                               utils::fs::join(DATA_FOLDER, "face-detection-0104.bin"));
+    InputInfo::Ptr inputInfo = net.getInputsInfo()["image"];
+    inputInfo->getPreProcess().setResizeAlgorithm(ResizeAlgorithm::RESIZE_BILINEAR);
+    inputInfo->setLayout(Layout::NHWC);
+    inputInfo->setPrecision(Precision::U8);
+    outputName = net.getOutputsInfo().begin()->first;
+
+    // Initialize runnable object on CPU device
+    ExecutableNetwork execNet = ie.LoadNetwork(net, "CPU");
+
+    // Create a single processing thread
+    req = execNet.CreateInferRequest();
+}
+
+Blob::Ptr wrapMatToBlob(const Mat& m) {
+    CV_Assert(m.depth() == CV_8U);
+    std::vector<size_t> dims = { 1, (size_t)m.channels(), (size_t)m.rows, (size_t)m.cols };
+    return make_shared_blob<uint8_t>(TensorDesc(Precision::U8, dims, Layout::NHWC),
+        m.data);
 }
 
 
@@ -29,8 +47,34 @@ void Detector::detect(const cv::Mat& image,
 void nms(const std::vector<cv::Rect>& boxes, const std::vector<float>& probabilities,
          float threshold, std::vector<unsigned>& indices) {
     CV_Error(Error::StsNotImplemented, "nms");
+    std::vector<cv::Rect> _boxes = boxes;
+    std::vector<float> _probabilities = probabilities;
+    while (_boxes.size() != 0)
+    {
+        float max_prob = 0;
+        int index_of_max_prob = 0;
+        for (int i = 0; i < _probabilities.size(); i++)
+            if (_probabilities[i] > max_prob)
+            {
+                max_prob = _probabilities[i];
+                index_of_max_prob = i;
+            }
+        cv::Rect max_box = _boxes[index_of_max_prob];
+        _boxes.erase(_boxes.begin() + index_of_max_prob);
+        _probabilities.erase(_probabilities.begin() + index_of_max_prob);
+        indices.push_back(index_of_max_prob);
+        for (int i = 0; i < _boxes.size(); i++)
+            if (iou(_boxes[i], max_box) > threshold)
+            {
+                _boxes.erase(_boxes.begin() + i);
+                _probabilities.erase(_probabilities.begin() + i);
+            }
+    }
 }
 
 float iou(const cv::Rect& a, const cv::Rect& b) {
-    CV_Error(Error::StsNotImplemented, "iou");
+    float in = (a & b).area();
+    float un = a.area() + b.area() - (a & b).area();
+    float ratio = in / un;
+    return ratio;
 }
